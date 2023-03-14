c8b561df6ecce4fc6f5923385d1c8c6d417929f6
gabrieldeori <deori.pessoal@gmail.com>
2023-03-14 18:59:29 -0300

    fix: ARRUMANDO UM PROBLEMA DE 3, escrever dbname em lowercase

--- login/classes/DB.php
+++ login/classes/DB.php
@@ -6,11 +6,12 @@
     public static function instanciar(){
       if(!isset(self::$pdo)) {
         try {
-          self::$pdo = new PDO('mysql:host='.SERVIDOR.';DBNAME='.BANCO,USUARIO,SENHA);
+          self::$pdo = new PDO('mysql:host='.SERVIDOR.';dbname='.BANCO,USUARIO,SENHA);
           self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+          echo "<br> CONECTADO <br>";
         } catch (PDOException $error) {
-          echo "Falha ao se conectar com o banco: ".$error->getMessage();
+          echo "<br> Falha ao se conectar com o banco: <br>".$error->getMessage();
         }
       }
       return self::$pdo;
