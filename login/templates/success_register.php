b5a3db27fff0f690c2b24ea5b453aeefdf90f53b
gabrieldeori <deori.pessoal@gmail.com>
2023-03-14 23:05:02 -0300

    feat: Adicionando template sucesso registro

---
+++ login/templates/success_register.php
@@ -0,0 +1,7 @@
+<?php if (isset($_GET['result']) && $_GET['result'] === 'ok') { ?>
+  <div
+    class="sucesso animate__animated animate__rubberBand"
+  >
+    Cadastrado com sucesso!
+  </div>
+<?php } ?>
