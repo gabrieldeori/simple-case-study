562ad36b4f6a219126acf562fee95cf524491a0b
gabrieldeori <deori.pessoal@gmail.com>
2023-03-16 22:10:45 -0300

    feat: pupulando Crud com funcao create

---
+++ php-agenda/classes/Crud.php
@@ -0,0 +1,31 @@
+<?php
+  require('../db/DB.php');
+
+  class Crud extends DB {
+    protected string $tabela;
+    protected string $query;
+    protected array $propArray;
+    protected array $erro;
+
+    public function create() {
+      $sql = "INSERT INTO $this->tabela VALUES $this->query";
+      $sql = DB::prepare($sql);
+      $registered = $sql->execute($this->propArray);
+
+      if($registered) {
+        return $registered;
+      }
+      $this->erro["erro_geral"] = "Ocorreu um erro interno no banco!";
+    }
+
+    public function findById($id) {
+      $sql = "SELECT * FROM $this->tabela WHERE id=? LIMIT 1";
+      $sql = DB::prepare($sql);
+      $sql->execute(array($id));
+      return $sql->fetch();
+    }
+
+    public function updateById($id) {
+    }
+  }
+?>
