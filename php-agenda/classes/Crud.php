fe35aa3efa847bc046a0759ab7836fd4cf99841e
gabrieldeori <deori.pessoal@gmail.com>
2023-03-17 10:26:18 -0300

    feat: findById para findBy

--- php-agenda/classes/Crud.php
+++ php-agenda/classes/Crud.php
@@ -3,12 +3,13 @@
 
   class Crud extends DB {
     protected string $tabela;
-    protected string $query;
+    protected string $values;
+    protected string $wheres;
     protected array $propArray;
     protected array $erro;
 
     public function create() {
-      $sql = "INSERT INTO $this->tabela VALUES $this->query";
+      $sql = "INSERT INTO $this->tabela VALUES $this->values";
       $sql = DB::prepare($sql);
       $registered = $sql->execute($this->propArray);
 
@@ -19,10 +20,10 @@
       $this->erro["erro_geral"] = "Ocorreu um erro interno no banco!";
     }
 
-    public function findById($id) {
-      $sql = "SELECT * FROM $this->tabela WHERE id=? LIMIT 1";
+    public function findBy() {
+      $sql = "SELECT * FROM $this->tabela WHERE $this->wheres LIMIT 1";
       $sql = DB::prepare($sql);
-      $sql->execute(array($id));
+      $sql->execute($this->propArray);
       return $sql->fetch();
     }
 
